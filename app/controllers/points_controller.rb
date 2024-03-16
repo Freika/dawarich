4680b80284b71a792329eb093a798c44ec75da23
Eugene Burmakin <iamfrey@gmail.com>
2024-03-16 22:51:46 +0100

    Fix points controller to filter by start_at and end_at

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -6,11 +6,11 @@ class PointsController < ApplicationController
     end_at = params[:end_at]&.to_datetime.to_i
 
     @points =
-      if start_at && end_at
-        Point.where('timestamp >= ? AND timestamp <= ?', start_at, end_at).order(timestamp: :asc)
+      if start_at.positive? && end_at.positive?
+        Point.where('timestamp >= ? AND timestamp <= ?', start_at, end_at)
       else
-        Point.all.order(timestamp: :asc)
-      end
+        Point.all
+      end.order(timestamp: :asc)
 
     @countries_and_cities = @points.group_by(&:country).transform_values { _1.pluck(:city).uniq.compact }
     @coordinates = @points.pluck(:latitude, :longitude).map { [_1.to_f, _2.to_f] }
