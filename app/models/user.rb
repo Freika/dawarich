2e6d05aa405f49fd5bb3bf0701d20e0276d08462
Eugene Burmakin <iamfrey@gmail.com>
2024-03-23 21:16:11 +0100

    Add nice statistics

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   has_many :imports, dependent: :destroy
   has_many :points, through: :imports
   has_many :stats
+
+  def total_km
+    Stat.where(user: self).sum(:distance)
+  end
+
+  def total_countries
+    Stat.where(user: self).pluck(:toponyms).flatten.uniq.size
+  end
+
+  def total_cities
+    Stat.where(user: self).pluck(:toponyms).flatten.size
+  end
+
 end
