8c70dd484af3a3ed5590257340f85080c2b72d9e
Eugene Burmakin <iamfrey@gmail.com>
2024-05-18 11:48:22 +0200

    Add link to date of city visit to right sidebar

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class PointsController < ApplicationController
   before_action :authenticate_user!
 
--- app/helpers/points_helper.rb
+++ app/helpers/points_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module PointsHelper
+  def link_to_date(timestamp)
+    datetime = Time.zone.at(timestamp)
+
+    link_to points_path(start_at: datetime.beginning_of_day, end_at: datetime.end_of_day), \
+            class: 'underline hover:no-underline' do
+      datetime.strftime('%d.%m.%Y')
+    end
+  end
 end
