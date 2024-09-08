ca571b8ceaa34fcad48c288f3221a198c532bd00
Eugene Burmakin <iamfrey@gmail.com>
2024-09-08 17:04:12 +0200

    Fix import geodata spec

--- spec/services/immich/import_geodata_spec.rb
+++ spec/services/immich/import_geodata_spec.rb
@@ -12,24 +12,18 @@ RSpec.describe Immich::ImportGeodata do
     let(:immich_data) do
       [
         {
-          "assets": [
-            {
-              "exifInfo": {
-                "dateTimeOriginal": '2022-12-31T23:17:06.170Z',
-                "latitude": 52.0000,
-                "longitude": 13.0000
-              }
-            },
-            {
-              "exifInfo": {
-                "dateTimeOriginal": '2022-12-31T23:21:53.140Z',
-                "latitude": 52.0000,
-                "longitude": 13.0000
-              }
-            }
-          ],
-          "title": '1 year ago',
-          "yearsAgo": 1
+          "exifInfo": {
+            "dateTimeOriginal": '2022-12-31T23:17:06.170Z',
+            "latitude": 52.0000,
+            "longitude": 13.0000
+          }
+        },
+        {
+          "exifInfo": {
+            "dateTimeOriginal": '2022-12-31T23:21:53.140Z',
+            "latitude": 52.0000,
+            "longitude": 13.0000
+          }
         }
       ].to_json
     end
@@ -38,7 +32,7 @@ RSpec.describe Immich::ImportGeodata do
       before do
         stub_request(
           :any,
-          %r{http://immich\.app/api/assets/memory-lane\?day=(1[0-9]|2[0-9]|3[01]|[1-9])&month=(1[0-2]|[1-9])}
+          %r{http://immich\.app/api/timeline/bucket\?size=MONTH&timeBucket=(19[7-9][0-9]|20[0-9]{2})-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])}
         ).to_return(status: 200, body: immich_data, headers: {})
       end
 
