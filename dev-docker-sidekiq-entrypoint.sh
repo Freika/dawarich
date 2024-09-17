6f658562f6331ef4b0da830b212101d4afd04bd3
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2024-09-17 18:53:10 -0400

    add support for alternate postgres ports and db names in docker

--- dev-docker-sidekiq-entrypoint.sh
+++ dev-docker-sidekiq-entrypoint.sh
@@ -4,8 +4,15 @@ set -e
 
 echo "Environment: $RAILS_ENV"
 
+# set env var defaults
+DATABASE_HOST=${DATABASE_HOST:-"dawarich_db"}
+DATABASE_PORT=${DATABASE_PORT:-5432}
+DATABASE_USER=${DATABASE_USER:-"postgres"}
+DATABASE_PASSWORD=${DATABASE_PASSWORD:-"password"}
+DATABASE_NAME=${DATABASE_NAME:-"dawarich_development"}
+
 # Wait for the database to be ready
-until nc -zv $DATABASE_HOST 5432; do
+until nc -zv $DATABASE_HOST ${DATABASE_PORT:-5432}; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 1
 done
