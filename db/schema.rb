3cc33a86ef378d91bbaeb3afc1a916dc5f202fa3
Eugene Burmakin <iamfrey@gmail.com>
2024-10-22 12:05:28 +0200

    Add points rendering mode to settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_092405) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_22_092405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_092405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_areas_on_user_id"
+  end
+
+  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
   end
 
   create_table "exports", force: :cascade do |t|
