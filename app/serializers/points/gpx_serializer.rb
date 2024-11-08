a5198f3a14deabad8f272211cfd3bfd8f326b14e
Eugene Burmakin <iamfrey@gmail.com>
2024-11-08 13:45:51 +0100

    Update gpx serializer to make it a valid gpx file

--- app/serializers/points/gpx_serializer.rb
+++ app/serializers/points/gpx_serializer.rb
@@ -1,26 +1,33 @@
 # frozen_string_literal: true
 
 class Points::GpxSerializer
-  def initialize(points)
+  def initialize(points, name)
     @points = points
+    @name = name
   end
 
   def call
-    gpx = GPX::GPXFile.new
+    gpx_file = GPX::GPXFile.new(name: "dawarich_#{name}")
+    track = GPX::Track.new(name: "dawarich_#{name}")
+
+    gpx_file.tracks << track
+
+    track_segment = GPX::Segment.new
+    track.segments << track_segment
 
     points.each do |point|
-      gpx.waypoints << GPX::Waypoint.new(
+      track_segment.points << GPX::TrackPoint.new(
         lat: point.latitude.to_f,
         lon: point.longitude.to_f,
-        time: point.recorded_at,
-        ele: point.altitude.to_f
+        elevation: point.altitude.to_f,
+        time: point.recorded_at
       )
     end
 
-    gpx
+    gpx_file.to_s.sub('<gpx', '<gpx xmlns="http://www.topografix.com/GPX/1/1"')
   end
 
   private
 
-  attr_reader :points
+  attr_reader :points, :name
 end
