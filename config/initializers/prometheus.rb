55e24f3adbeca964b06f00a1810f758620441aed
Eugene Burmakin <iamfrey@gmail.com>
2024-11-20 22:00:24 +0100

    Change default number of Puma workers to 2

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,12 +1,15 @@
-# in config/initializers/prometheus.rb
-if Rails.env != "test" && ENV['PROMETHEUS_EXPORTER_ENABLED'].to_s == 'true'
+# frozen_string_literal: true
+
+if !Rails.env.test? && ENV['PROMETHEUS_EXPORTER_ENABLED'].to_s == 'true'
   require 'prometheus_exporter/middleware'
   require 'prometheus_exporter/instrumentation'
 
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 
-    # this reports basic process stats like RSS and GC info, type master
-  # means it is instrumenting the master process
-  PrometheusExporter::Instrumentation::Process.start(type: "master")
+  # this reports basic process stats like RSS and GC info
+  PrometheusExporter::Instrumentation::Process.start(type: 'web')
+
+  # Add ActiveRecord instrumentation
+  PrometheusExporter::Instrumentation::ActiveRecord.start
 end
