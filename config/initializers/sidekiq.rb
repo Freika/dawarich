342f025bf20eb52edebf6079af83e495fccd294b
Eugene Burmakin <iamfrey@gmail.com>
2024-11-20 21:05:02 +0100

    Add port 9394 to docker-compose.yml for Prometheus exporter

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,21 +3,19 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 
-  if ENV.fetch('PROMETHEUS_EXPORTER_ENABLED', false)
-    require 'prometheus_exporter/instrumentation'
+  require 'prometheus_exporter/instrumentation'
 
-    config.server_middleware do |chain|
-      chain.add PrometheusExporter::Instrumentation::Sidekiq
-    end
+  config.server_middleware do |chain|
+    chain.add PrometheusExporter::Instrumentation::Sidekiq
+  end
 
-    config.death_handlers << PrometheusExporter::Instrumentation::Sidekiq.death_handler
+  config.death_handlers << PrometheusExporter::Instrumentation::Sidekiq.death_handler
 
-    config.on :startup do
-      PrometheusExporter::Instrumentation::Process.start type: 'sidekiq'
-      PrometheusExporter::Instrumentation::SidekiqProcess.start
-      PrometheusExporter::Instrumentation::SidekiqQueue.start
-      PrometheusExporter::Instrumentation::SidekiqStats.start
-    end
+  config.on :startup do
+    PrometheusExporter::Instrumentation::Process.start type: 'sidekiq'
+    PrometheusExporter::Instrumentation::SidekiqProcess.start
+    PrometheusExporter::Instrumentation::SidekiqQueue.start
+    PrometheusExporter::Instrumentation::SidekiqStats.start
   end
 end
 
