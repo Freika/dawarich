cf5b414ae46d1b3f324518049cda2c666c63a895
Eugene Burmakin <iamfrey@gmail.com>
2024-11-20 17:16:26 +0100

    Use single condition in the prometheus initializer file

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,15 +1,12 @@
 # in config/initializers/prometheus.rb
 if Rails.env != "test"
   require 'prometheus_exporter/middleware'
+  require 'prometheus_exporter/instrumentation'
 
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
-end
 
-if Rails.env != "test"
-  require 'prometheus_exporter/instrumentation'
-
-  # this reports basic process stats like RSS and GC info, type master
+    # this reports basic process stats like RSS and GC info, type master
   # means it is instrumenting the master process
   PrometheusExporter::Instrumentation::Process.start(type: "master")
 end
