6093f4cb33bb5afe0d78a32468cf08471fcd37d2
Eugene Burmakin <iamfrey@gmail.com>
2024-11-24 15:28:09 +0100

    Return sidekiq_rate_limiting

--- app/jobs/reverse_geocoding_job.rb
+++ app/jobs/reverse_geocoding_job.rb
@@ -6,7 +6,7 @@ class ReverseGeocodingJob < ApplicationJob
   def perform(klass, id)
     return unless REVERSE_GEOCODING_ENABLED
 
-    sleep 1 if PHOTON_API_HOST == 'photon.komoot.io'
+    rate_limit_for_photon_api
 
     data_fetcher(klass, id).call
   end
@@ -15,5 +15,11 @@ class ReverseGeocodingJob < ApplicationJob
 
   def data_fetcher(klass, id)
     "ReverseGeocoding::#{klass.pluralize.camelize}::FetchData".constantize.new(id)
+  end
+
+  def rate_limit_for_photon_api
+    return unless PHOTON_API_HOST == 'photon.komoot.io'
+
+    sleep 1 if PHOTON_API_HOST == 'photon.komoot.io'
   end
 end
