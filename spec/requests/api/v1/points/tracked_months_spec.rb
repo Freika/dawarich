81dc03f7c970bdeadc2a92d112e2efa6fb1e6b00
Eugene Burmakin <iamfrey@gmail.com>
2024-12-16 15:22:34 +0100

    Add swagger specs for tracked months and visited cities

--- spec/requests/api/v1/points/tracked_months_spec.rb
+++ spec/requests/api/v1/points/tracked_months_spec.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Points::TrackedMonths", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Points::TrackedMonths', type: :request do
+  describe 'GET /index' do
+    let(:user) { create(:user) }
+
+    it 'returns tracked months' do
+      get "/api/v1/points/tracked_months?api_key=#{user.api_key}"
+
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
