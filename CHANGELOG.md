10afb3fbc262cc5a7583a5ec87dc21bebefa1257
Eugene Burmakin <iamfrey@gmail.com>
2025-01-05 21:40:59 +0100

    Fix Photon API for patreon supporters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+# 0.21.4 - 2025-01-05
+
+### Fixed
+
+- Fixed a bug where Photon API for patreon supporters was not being used for reverse geocoding.
+
 # 0.21.3 - 2025-01-04
 
 ### Added
--- config/initializers/01_constants.rb
+++ config/initializers/01_constants.rb
@@ -3,6 +3,7 @@
 MIN_MINUTES_SPENT_IN_CITY = ENV.fetch('MIN_MINUTES_SPENT_IN_CITY', 60).to_i
 REVERSE_GEOCODING_ENABLED = ENV.fetch('REVERSE_GEOCODING_ENABLED', 'true') == 'true'
 PHOTON_API_HOST = ENV.fetch('PHOTON_API_HOST', nil)
+PHOTON_API_KEY = ENV.fetch('PHOTON_API_KEY', nil)
 PHOTON_API_USE_HTTPS = ENV.fetch('PHOTON_API_USE_HTTPS', 'true') == 'true'
 DISTANCE_UNIT = ENV.fetch('DISTANCE_UNIT', 'km').to_sym
 APP_VERSION = File.read('.app_version').strip
