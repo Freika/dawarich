25442b4622b25c151eec95043dfdf788586731cb
Eugene Burmakin <iamfrey@gmail.com>
2025-05-31 18:33:01 +0200

    Remove version cache initializer

--- config/initializers/02_version_cache.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Defer cache operations until after initialization to avoid SolidCache loading issues
-Rails.application.config.after_initialize do
-  # Skip cache clearing when running the Rails console
-  unless defined?(Rails::Console) || File.basename($PROGRAM_NAME) == 'rails' && ARGV.include?('console')
-    Rails.cache.delete('dawarich/app-version-check') if Rails.cache.respond_to?(:delete)
-  end
-end
