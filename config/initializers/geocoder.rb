3d26a4962758b2932f8d4686f96efb6b8b893b4a
Eugene Burmakin <iamfrey@gmail.com>
2025-06-09 14:10:49 +0200

    Fix redis urls

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -4,7 +4,7 @@ settings = {
   debug_mode: true,
   timeout: 5,
   units: :km,
-  cache: Redis.new,
+  cache: Redis.new(url: ENV['REDIS_URL']),
   always_raise: :all,
   http_headers: {
     'User-Agent' => "Dawarich #{APP_VERSION} (https://dawarich.app)"
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: "#{ENV['REDIS_URL']}/1" }
   config.logger = Sidekiq::Logger.new($stdout)
 
   if ENV['PROMETHEUS_EXPORTER_ENABLED'].to_s == 'true'
