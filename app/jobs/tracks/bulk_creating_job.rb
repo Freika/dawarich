244fb2b192e6d5b1035d8eeb6bfc267760c9aab1
Eugene Burmakin <iamfrey@gmail.com>
2025-07-12 23:04:15 +0200

    Move bulk track creation to service

--- app/jobs/tracks/bulk_creating_job.rb
+++ app/jobs/tracks/bulk_creating_job.rb
@@ -17,32 +17,6 @@ class Tracks::BulkCreatingJob < ApplicationJob
   sidekiq_options retry: false
 
   def perform(start_at: nil, end_at: 1.day.ago.end_of_day, user_ids: [])
-    users = user_ids.any? ? User.active.where(id: user_ids) : User.active
-    end_at = end_at.to_datetime
-
-    users.find_each do |user|
-      next if user.tracked_points.empty?
-
-      # Start from the end of the last track, or from the beginning if no tracks exist
-      user_start_at = start_at&.to_datetime || start_time(user)
-
-      next unless user.tracked_points.where(timestamp: user_start_at.to_i..end_at.to_i).exists?
-
-      Tracks::CreateJob.perform_later(user.id, start_at: user_start_at, end_at: end_at, cleaning_strategy: :daily)
-    end
-  end
-
-  private
-
-  def start_time(user)
-    # Find the latest track for this user
-    latest_track = user.tracks.order(end_at: :desc).first
-
-    if latest_track
-      latest_track.end_at
-    else
-      oldest_point = user.tracked_points.order(:timestamp).first
-      oldest_point ? Time.zone.at(oldest_point.timestamp) : 1.day.ago.beginning_of_day
-    end
+    Tracks::BulkTrackCreator.new(start_at:, end_at:, user_ids:).call
   end
 end
