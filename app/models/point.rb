0bfddd932fbdee8a1b974a049fe1c88d4186cdb6
Eugene Burmakin <iamfrey@gmail.com>
2025-07-22 20:28:46 +0200

    Disable specs for track generation

--- app/models/point.rb
+++ app/models/point.rb
@@ -33,7 +33,7 @@ class Point < ApplicationRecord
   after_create :async_reverse_geocode, if: -> { DawarichSettings.store_geodata? && !reverse_geocoded? }
   after_create :set_country
   after_create_commit :broadcast_coordinates
-  after_create_commit :trigger_incremental_track_generation, if: -> { import_id.nil? }
+  # after_create_commit :trigger_incremental_track_generation, if: -> { import_id.nil? }
   # after_commit :recalculate_track, on: :update, if: -> { track.present? }
 
   def self.without_raw_data
