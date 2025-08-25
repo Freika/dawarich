ce5ae8dc70f2600af72df3f23768314fb8a1cb62
Eugene Burmakin <iamfrey@gmail.com>
2025-08-25 23:35:11 +0200

    Fix logout bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+import "@rails/ujs"
 import "@rails/actioncable"
 import "controllers"
 import "@hotwired/turbo-rails"
