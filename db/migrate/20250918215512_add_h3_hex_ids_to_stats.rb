f223feb854503b0bd46d33f4fb2ae988a4c7443b
Eugene Burmakin <iamfrey@gmail.com>
2025-09-20 15:53:27 +0200

    Add safety_assured block for index creation

--- db/migrate/20250918215512_add_h3_hex_ids_to_stats.rb
+++ db/migrate/20250918215512_add_h3_hex_ids_to_stats.rb
@@ -5,8 +5,10 @@ class AddH3HexIdsToStats < ActiveRecord::Migration[8.0]
 
   def change
     add_column :stats, :h3_hex_ids, :jsonb, default: {}
-    add_index :stats, :h3_hex_ids, using: :gin,
-              where: "(h3_hex_ids IS NOT NULL AND h3_hex_ids != '{}'::jsonb)",
-              algorithm: :concurrently
+    safety_assured do
+      add_index :stats, :h3_hex_ids, using: :gin,
+                where: "(h3_hex_ids IS NOT NULL AND h3_hex_ids != '{}'::jsonb)",
+                algorithm: :concurrently
+    end
   end
 end
