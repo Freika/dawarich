a53bd0a4bddf2289d50c700f708416a764962706
Eugene Burmakin <iamfrey@gmail.com>
2025-09-26 19:48:52 +0200

    Add staging environment configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   # Skip DNS rebinding protection for the health check endpoint.
-  config.host_authorization = { exclude: ->(request) { request.path == "/api/v1/health" } }
+  config.host_authorization = { exclude: ->(request) { request.path == '/api/v1/health' } }
   hosts = ENV.fetch('APPLICATION_HOSTS', 'localhost').split(',')
 
   config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
