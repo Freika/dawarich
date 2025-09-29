698198db4b75d11cf2f5f2dc682977656a6d34c4
Eugene Burmakin <iamfrey@gmail.com>
2025-09-30 00:45:09 +0200

    Update navbar for better responsiveness

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[8.0].define(version: 2025_09_28_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-  enable_extension "pgcrypto"
   enable_extension "postgis"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
