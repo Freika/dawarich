e86264a4f4f4aafb00ae731299d5972d05957580
Eugene Burmakin <iamfrey@gmail.com>
2025-10-02 21:54:18 +0200

    Change asset compilation setting in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.assets.content_type = {
     geojson: 'application/geo+json'
