97d52f9edc078ff4e07815de2d995be30bfc91af
Eugene Burmakin <iamfrey@gmail.com>
2025-12-06 20:36:52 +0100

    Update maplibre controller

--- app/controllers/map/maplibre_controller.rb
+++ app/controllers/map/maplibre_controller.rb
@@ -8,26 +8,26 @@ module Map
       @end_at = parsed_end_at
     end
 
-  private
+    private
 
-  def start_at
-    return Time.zone.parse(params[:start_at]).to_i if params[:start_at].present?
+    def start_at
+      return Time.zone.parse(params[:start_at]).to_i if params[:start_at].present?
 
-    Time.zone.today.beginning_of_day.to_i
-  end
+      Time.zone.today.beginning_of_day.to_i
+    end
 
-  def end_at
-    return Time.zone.parse(params[:end_at]).to_i if params[:end_at].present?
+    def end_at
+      return Time.zone.parse(params[:end_at]).to_i if params[:end_at].present?
 
-    Time.zone.today.end_of_day.to_i
-  end
+      Time.zone.today.end_of_day.to_i
+    end
 
-  def parsed_start_at
-    Time.zone.at(start_at)
-  end
+    def parsed_start_at
+      Time.zone.at(start_at)
+    end
 
-  def parsed_end_at
-    Time.zone.at(end_at)
-  end
+    def parsed_end_at
+      Time.zone.at(end_at)
+    end
   end
 end
