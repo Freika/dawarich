2f11003c2947527ffb6db30378901400a65f4e7b
Eugene Burmakin <iamfrey@gmail.com>
2025-12-30 19:02:38 +0100

    Fix migrations and data verification to remove safety_assured blocks and handle missing points gracefully.

--- db/migrate/20251227223614_change_digests_distance_to_bigint.rb
+++ db/migrate/20251227223614_change_digests_distance_to_bigint.rb
@@ -4,10 +4,10 @@ class ChangeDigestsDistanceToBigint < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def up
-    safety_assured { change_column :digests, :distance, :bigint, null: false, default: 0 }
+    change_column :digests, :distance, :bigint, null: false, default: 0
   end
 
   def down
-    safety_assured { change_column :digests, :distance, :integer, null: false, default: 0 }
+    change_column :digests, :distance, :integer, null: false, default: 0
   end
 end
