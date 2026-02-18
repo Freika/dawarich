cd22b892cd1675779ecf9cf10970da191632b126
Eugene Burmakin <iamfrey@gmail.com>
2026-02-18 21:24:26 +0100

    Remove creeped diff

--- db/schema.rb
+++ db/schema.rb
@@ -175,22 +175,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_17_000001) do
     t.index ["user_id"], name: "index_imports_on_user_id"
   end
 
-  create_table "notes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "title"
-    t.geography "lonlat", limit: {srid: 4326, type: "st_point", geographic: true}
-    t.string "attachable_type"
-    t.bigint "attachable_id"
-    t.datetime "noted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index "attachable_type, attachable_id, ((noted_at)::date)", name: "index_notes_on_attachable_and_noted_date", unique: true, where: "(attachable_id IS NOT NULL)"
-    t.index ["attachable_type", "attachable_id"], name: "index_notes_on_attachable_type_and_attachable_id"
-    t.index ["lonlat"], name: "index_notes_on_lonlat", using: :gist
-    t.index ["user_id", "noted_at"], name: "index_notes_on_user_id_and_noted_at"
-    t.index ["user_id"], name: "index_notes_on_user_id"
-  end
-
   create_table "notifications", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
@@ -563,7 +547,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_17_000001) do
   add_foreign_key "family_invitations", "users", column: "invited_by_id"
   add_foreign_key "family_memberships", "families"
   add_foreign_key "family_memberships", "users"
-  add_foreign_key "notes", "users"
   add_foreign_key "notifications", "users"
   add_foreign_key "place_visits", "places"
   add_foreign_key "place_visits", "visits"
