08d2ef46a417c3dd7523b8b250afb8f089e76343
Adrian de la Rosa <github@brick.email>
2026-03-22 16:20:04 +0100

    feat: add OutlierDetectionJob background job

---
+++ app/jobs/points/outlier_detection_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Points::OutlierDetectionJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id, start_at = nil, end_at = nil)
+    user = find_user_or_skip(user_id) || return
+    return unless user.safe_settings.outlier_detection_enabled?
+
+    parsed_start = start_at ? Time.parse(start_at.to_s) : nil
+    parsed_end = end_at ? Time.parse(end_at.to_s) : nil
+
+    count = Points::OutlierDetector.new(user, start_at: parsed_start, end_at: parsed_end).call
+
+    Rails.logger.info(
+      "#{self.class.name}: Flagged #{count} outlier points for user #{user_id}"
+    )
+  end
+end
---
+++ spec/jobs/points/outlier_detection_job_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Points::OutlierDetectionJob, type: :job do
+  let(:user) { create(:user) }
+
+  describe '#perform' do
+    it 'calls the OutlierDetector service' do
+      detector = instance_double(Points::OutlierDetector, call: 0)
+      allow(Points::OutlierDetector).to receive(:new)
+        .with(user, start_at: nil, end_at: nil)
+        .and_return(detector)
+
+      described_class.new.perform(user.id)
+
+      expect(detector).to have_received(:call)
+    end
+
+    it 'passes date range when provided' do
+      start_at = '2024-05-01T00:00:00Z'
+      end_at = '2024-05-01T23:59:59Z'
+
+      detector = instance_double(Points::OutlierDetector, call: 0)
+      allow(Points::OutlierDetector).to receive(:new)
+        .with(user, start_at: Time.parse(start_at), end_at: Time.parse(end_at))
+        .and_return(detector)
+
+      described_class.new.perform(user.id, start_at, end_at)
+
+      expect(detector).to have_received(:call)
+    end
+
+    it 'skips if user not found' do
+      expect(Points::OutlierDetector).not_to receive(:new)
+      described_class.new.perform(-1)
+    end
+
+    it 'skips if outlier detection is disabled' do
+      user.settings['outlier_detection_enabled'] = false
+      user.save!
+
+      expect(Points::OutlierDetector).not_to receive(:new)
+      described_class.new.perform(user.id)
+    end
+  end
+end
