a5f9afd6de5eec61544a2db568a18a7ffcab1d2e
Eugene Burmakin <iamfrey@gmail.com>
2026-03-26 20:58:55 +0100

    Update migrations

--- db/migrate/20260326152648_add_demo_to_imports.rb
+++ db/migrate/20260326152648_add_demo_to_imports.rb
@@ -2,6 +2,6 @@
 
 class AddDemoToImports < ActiveRecord::Migration[8.0]
   def change
-    add_column :imports, :demo, :boolean, default: false, null: false
+    add_column :imports, :demo, :boolean, default: false, null: false unless column_exists?(:imports, :demo)
   end
 end
