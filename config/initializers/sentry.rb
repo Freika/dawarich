b0ff86399e8866b7e56cbb09ec93c05a4e244f8f
Eugene Burmakin <iamfrey@gmail.com>
2026-05-02 12:53:48 +0200

    feat: enable Sentry Logs with Rails.logger broadcast
    Mirrors the manager-app setup:
    - Sets config.enable_logs = true on Sentry init (sentry-ruby 6.2.0
      already supports it).
    - Adds before_send_log to redact sensitive attributes (password,
      token, api_key, etc.) and replace emails in both attributes and
      log bodies with [FILTERED] / [EMAIL].
    - Adds SentryLogsLogger that forwards Rails.logger output to
      Sentry.logger, broadcast onto Rails.logger after initialization.
      Includes a Thread-local guard to avoid recursion.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,5 +7,38 @@ Sentry.init do |config|
   config.dsn = SENTRY_DSN
   config.traces_sample_rate = 1.0
   config.profiles_sample_rate = 1.0
-  # config.enable_logs = true
+  config.enable_logs = true
+
+  sensitive_keys = %w[
+    password password_confirmation token api_key secret authorization
+    access_token refresh_token otp ssn credit_card card_number cvv
+  ]
+  email_pattern = /[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,}/
+
+  config.before_send_log = lambda do |log|
+    log.attributes.each do |key, value|
+      if sensitive_keys.any? { |k| key.to_s.downcase.include?(k) }
+        log.attributes[key] = '[FILTERED]'
+      elsif value.is_a?(String) && value.match?(email_pattern)
+        log.attributes[key] = value.gsub(email_pattern, '[EMAIL]')
+      end
+    end
+    log.body = log.body.gsub(email_pattern, '[EMAIL]') if log.body.is_a?(String)
+    log
+  end
+end
+
+require Rails.root.join('lib/sentry_logs_logger')
+
+Rails.application.config.after_initialize do
+  next unless Sentry.initialized?
+  next if Rails.logger.nil?
+
+  sentry_logger = SentryLogsLogger.new(level: ::Logger::INFO)
+
+  if Rails.logger.respond_to?(:broadcast_to)
+    Rails.logger.broadcast_to(sentry_logger)
+  else
+    Rails.logger.extend(ActiveSupport::Logger.broadcast(sentry_logger))
+  end
 end
---
+++ lib/sentry_logs_logger.rb
@@ -0,0 +1,40 @@
+require 'logger'
+
+class SentryLogsLogger < ::Logger
+  SEVERITY_MAP = {
+    ::Logger::DEBUG => :debug,
+    ::Logger::INFO  => :info,
+    ::Logger::WARN  => :warn,
+    ::Logger::ERROR => :error,
+    ::Logger::FATAL => :fatal,
+    ::Logger::UNKNOWN => :info
+  }.freeze
+
+  def initialize(level: ::Logger::WARN)
+    super(File::NULL)
+    self.level = level
+  end
+
+  def add(severity, message = nil, progname = nil, &block)
+    severity ||= ::Logger::UNKNOWN
+    return true if severity < level
+    return true unless Sentry.initialized?
+
+    message = block.call if message.nil? && block
+    message = progname if message.nil?
+    return true if message.nil?
+
+    Thread.current[:sentry_logs_logger_in_progress] ||= false
+    return true if Thread.current[:sentry_logs_logger_in_progress]
+
+    Thread.current[:sentry_logs_logger_in_progress] = true
+    begin
+      Sentry.logger.public_send(SEVERITY_MAP[severity], message.to_s)
+    rescue StandardError
+      nil
+    ensure
+      Thread.current[:sentry_logs_logger_in_progress] = false
+    end
+    true
+  end
+end
