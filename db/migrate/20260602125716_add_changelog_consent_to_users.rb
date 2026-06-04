f93e67bcdcbbd4d736a17e37477f761e4e6e60a3
Eugene Burmakin <iamfrey@gmail.com>
2026-06-04 21:35:45 +0200

    fix: make changelog_consent migration idempotent
    Guard add_column with if_not_exists: true so re-running the migration is safe.

--- db/migrate/20260602125716_add_changelog_consent_to_users.rb
+++ db/migrate/20260602125716_add_changelog_consent_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddChangelogConsentToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :changelog_consent, :integer
+    add_column :users, :changelog_consent, :integer, if_not_exists: true
   end
 end
