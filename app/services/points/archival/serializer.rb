f7ac2566c9f9d9b141cfb9f26c8d943f437f640b
Eugene Burmakin <iamfrey@gmail.com>
2026-06-08 21:02:25 +0200

    feat: add Points::Archival::Serializer with EWKB round-trip

---
+++ app/services/points/archival/serializer.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module Points
+  module Archival
+    class Serializer
+      LONLAT = 'lonlat'
+
+      def self.columns
+        @columns ||= Point.column_names
+      end
+
+      def self.scalar_columns
+        @scalar_columns ||= columns - [LONLAT]
+      end
+
+      def self.jsonb_columns
+        @jsonb_columns ||= Point.columns_hash.select { |_, c| c.sql_type == 'jsonb' }.keys
+      end
+
+      def self.array_columns
+        @array_columns ||= Point.columns_hash.select { |_, c| c.array? }.keys
+      end
+
+      def self.dump(point)
+        attrs = point.attributes_before_type_cast.slice(*scalar_columns)
+        attrs[LONLAT] = ewkb_hex(point.id)
+        "#{attrs.to_json}\n"
+      end
+
+      def self.ewkb_hex(point_id)
+        Point.connection.select_value(
+          Point.sanitize_sql_array(
+            ["SELECT encode(ST_AsEWKB(lonlat::geometry), 'hex') FROM points WHERE id = ?", point_id]
+          )
+        )
+      end
+
+      def self.parse(line)
+        JSON.parse(line)
+      end
+
+      def self.insert_sql(rows)
+        cols = columns
+        col_list = cols.map { |c| Point.connection.quote_column_name(c) }.join(', ')
+        values = rows.map { |attrs| value_tuple(attrs, cols) }.join(', ')
+        <<~SQL.squish
+          INSERT INTO points (#{col_list}) VALUES #{values}
+          ON CONFLICT (id) DO NOTHING
+        SQL
+      end
+
+      def self.value_tuple(attrs, cols)
+        rendered = cols.map do |col|
+          val = attrs[col]
+          if col == LONLAT
+            val ? "ST_GeomFromEWKB(decode(#{Point.connection.quote(val)}, 'hex'))" : 'NULL'
+          elsif jsonb_columns.include?(col)
+            if val.nil?
+              'NULL'
+            else
+              json_str = val.is_a?(String) ? val : val.to_json
+              "#{Point.connection.quote(json_str)}::jsonb"
+            end
+          elsif array_columns.include?(col)
+            val.nil? ? 'NULL' : "ARRAY[#{Array(val).map { |e| Point.connection.quote(e) }.join(', ')}]::text[]"
+          else
+            Point.connection.quote(val)
+          end
+        end
+        "(#{rendered.join(', ')})"
+      end
+    end
+  end
+end
---
+++ spec/services/points/archival/serializer_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Points::Archival::Serializer do
+  let(:user) { create(:user) }
+  let!(:point) do
+    create(:point, user:, longitude: 13.404954, latitude: 52.520008, timestamp: 1_700_000_000,
+                   raw_data: { 'foo' => 'bar', 'n' => 42 })
+  end
+
+  it 'round-trips a point to a JSON line and back to identical attributes' do
+    line = described_class.dump(Point.where(id: point.id).first)
+    attrs = described_class.parse(line)
+
+    expect(attrs['id']).to eq(point.id)
+    expect(attrs['user_id']).to eq(user.id)
+    expect(attrs['timestamp']).to eq(point.timestamp)
+  end
+
+  it 'preserves geometry exactly through dump/parse/insert' do
+    line = described_class.dump(Point.where(id: point.id).first)
+    attrs = described_class.parse(line)
+
+    point.destroy!
+    Point.connection.execute(described_class.insert_sql([attrs]))
+
+    restored = Point.find(point.id)
+    expect(restored.lonlat.x).to be_within(1e-9).of(13.404954)
+    expect(restored.lonlat.y).to be_within(1e-9).of(52.520008)
+  end
+
+  it 'preserves jsonb columns exactly through dump/parse/insert' do
+    line = described_class.dump(Point.where(id: point.id).first)
+    attrs = described_class.parse(line)
+
+    point.destroy!
+    Point.connection.execute(described_class.insert_sql([attrs]))
+
+    expect(Point.find(point.id).raw_data).to eq('foo' => 'bar', 'n' => 42)
+  end
+end
