0dc92ed8566bfdd6535d2a8d14a9b8fc2cdcfa0f
Luke Picciau <luke@picciau.me>
2026-07-17 16:45:29 +1000

    remove date filter

--- app/controllers/map/fog_controller.rb
+++ app/controllers/map/fog_controller.rb
@@ -2,36 +2,12 @@
 
 module Map
   class FogController < ApplicationController
-    include SafeTimestampParser
-
     before_action :authenticate_user!
     layout 'map'
 
     def index
-      @start_at = parsed_start_at
-      @end_at = parsed_end_at
-    end
-
-    private
-
-    def start_at
-      return safe_timestamp(params[:start_at]) if params[:start_at].present?
-
-      Time.zone.today.beginning_of_day.to_i
-    end
-
-    def end_at
-      return safe_timestamp(params[:end_at]) if params[:end_at].present?
-
-      Time.zone.today.end_of_day.to_i
-    end
-
-    def parsed_start_at
-      Time.zone.at(start_at)
-    end
-
-    def parsed_end_at
-      Time.zone.at(end_at)
+      @start_at = Time.zone.parse('1970-01-01').beginning_of_day
+      @end_at = Time.zone.today.end_of_day
     end
   end
 end
