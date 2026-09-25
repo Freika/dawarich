ed47271ead80b6f767c7b06b4c8080322a59cea1
Eugene Burmakin <iamfrey@gmail.com>
2026-09-25 15:44:14 +0200

    chore: refine Dokploy staging readiness and keep runbook in AFFiNE

--- docker/cloud-entrypoint.sh
+++ docker/cloud-entrypoint.sh
@@ -7,17 +7,7 @@ unset BUNDLE_PATH
 unset BUNDLE_BIN
 
 if [ "$(id -u)" = 0 ]; then
-  cloud_uid=32767
-  cloud_gid=32767
-  watched_dir="${APP_PATH:-/var/app}/tmp/imports/watched"
-
-  # The named volume starts root-owned. The watcher must be able to read and
-  # remove files placed there, including files restored during cutover.
-  mkdir -p "$watched_dir"
-  if [ "$(stat -c '%u:%g' "$watched_dir")" != "$cloud_uid:$cloud_gid" ]; then
-    chown -R "$cloud_uid:$cloud_gid" "$watched_dir"
-  fi
-  exec gosu "$cloud_uid:$cloud_gid" "$0" "$@"
+  exec gosu 32767:32767 "$0" "$@"
 fi
 
 exec bundle exec "$@"
